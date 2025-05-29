67909b988928468230072625eb517f0c669b7d22
ChrisPoon <chris@ChrisPoons-MacBook-Pro.local>
2025-05-29 08:52:18 -0700

    fix availability zone

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 # AWS EC2 Instance
 resource "aws_instance" "minecraft_server" {
   ami                    = "ami-04999cd8f2624f834"
-  instance_type          = "t2.large"
+  instance_type          = "t3.large"
   key_name               = "minecraft_key"
   subnet_id              = aws_subnet.minecraft_subnet.id
   vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "available" {}
+
 # AWS VPC minecraft
 resource "aws_vpc" "minecraft_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -10,7 +12,8 @@ resource "aws_subnet" "minecraft_subnet" {
   vpc_id                  = aws_vpc.minecraft_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  
   tags = {
     Name = "minecraft_subnet"
   }
