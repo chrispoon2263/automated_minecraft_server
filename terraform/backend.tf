e9a7e721363f1c8d16a914da9d6db7134c1568e6
ChrisPoon <chris@ChrisPoons-MacBook-Pro.local>
2025-05-29 18:19:15 -0700

    added s3 state bucket

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "chris-terraform-minecraft-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
