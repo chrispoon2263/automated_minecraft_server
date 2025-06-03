98424413d9d1b90892e0b555771b321b1a38d9a1
ChrisPoon <chris@10-249-104-89.wireless.oregonstate.edu>
2025-06-03 09:13:34 -0700

    reordered ssh key to be first

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,16 +4,6 @@
 if aws s3api head-bucket --bucket chris-terraform-minecraft-bucket 2>/dev/null; then
   echo "Bucket already exists"
 else
-  # provision an S3 bucket on AWS
-  echo "Creating S3 bucket, initial Minecraft server files, and terraformt.tfstate..."
-  aws s3api create-bucket --bucket chris-terraform-minecraft-bucket \
-    --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
-  # Copy terraform on local machine to S3 bucket
-  aws s3 cp ./terraform/terraform.tfstate s3://chris-terraform-minecraft-bucket/terraform.tfstate --region us-west-2
-
-  # Copy minecraft server files to S3 bucket
-  aws s3 cp ./server_files s3://chris-terraform-minecraft-bucket/server_files --region us-west-2 --recursive
-
   # Create the AWS key pair and place into ~/.ssh/mineraft_key.pm
   echo "Generating AWS ssh key pair"
   mkdir -p ~/.ssh
@@ -23,6 +13,16 @@ else
     --query 'KeyMaterial' \
     --output text > ~/.ssh/minecraft_key.pem
   chmod 400 ~/.ssh/minecraft_key.pem
+  
+  # provision an S3 bucket on AWS
+  echo "Creating S3 bucket, initial Minecraft server files, and terraformt.tfstate..."
+  aws s3api create-bucket --bucket chris-terraform-minecraft-bucket \
+    --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+  # Copy terraform on local machine to S3 bucket
+  aws s3 cp ./terraform/terraform.tfstate s3://chris-terraform-minecraft-bucket/terraform.tfstate --region us-west-2
+
+  # Copy minecraft server files to S3 bucket
+  aws s3 cp ./server_files s3://chris-terraform-minecraft-bucket/server_files --region us-west-2 --recursive
 fi
 
 # Bandaid solution using environment variables to sync s3
